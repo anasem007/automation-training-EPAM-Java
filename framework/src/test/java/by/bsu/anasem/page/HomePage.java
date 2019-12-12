830e91d2761244bf48e4d3f76094671f5ed6c7d5
Anastasiya Semenova <anastasiyasemen0.0va@gmail.com>
2019-12-12 15:48:11 +0300

    update page

--- framework/src/test/java/by/bsu/anasem/page/HomePage.java
+++ framework/src/test/java/by/bsu/anasem/page/HomePage.java
@@ -1,7 +1,10 @@
 package by.bsu.anasem.page;
 
+import by.bsu.anasem.model.CarBookingCriteria;
+import org.apache.log4j.Level;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -50,39 +53,101 @@ public class HomePage extends AbstractPage {
     @FindBy(xpath = "//input[@id='formsubmit']")
     private WebElement searchButton;
 
+    @FindBy(xpath = "//input[@id='driver-over-min-age']")
+    private WebElement driversAgeButton;
+
+    @FindBy(xpath = "//button[@id='rch-select-sign-in']")
+    private  WebElement selectSignIn;
+
+    @FindBy(xpath = "//input[@id='crmEmail']")
+    private WebElement email;
+
+    @FindBy(xpath = "//input[@id='crmPsw']")
+    private WebElement password;
+
+    @FindBy(xpath = "//input[@id='crmLogin']")
+    private WebElement login;
+
+    @FindBy(xpath = "//div[@id='showLoginErrorMessage']")
+    private WebElement errorMessage;
+
     public HomePage(WebDriver driver) {
         super(driver);
         PageFactory.initElements(this.driver, this);
     }
 
-    public HomePage inputPickUpLocation(String pickLoc) {
-        pickUpLocation.sendKeys(pickLoc);
+    public HomePage clickToSignInYourAccount(){
+        selectSignIn.click();
         return this;
     }
 
-    public HomePage inputDropOffLocation(String pickLoc) {
-        dropOffLocationButton.click();
+    public HomePage inputEmail(String emailAdress){
+        email.sendKeys(emailAdress);
+        return this;
+    }
+
+    public HomePage inputPassword(String userPassword){
+        password.sendKeys(userPassword);
+        return this;
+    }
+
+    public CarSelectionPage bookingCar(CarBookingCriteria carBookingCriteria) {
+        inputPickUpLocation(carBookingCriteria.getPickUpLocation())
+                .selectPickUpTime(carBookingCriteria.getPuHour(), carBookingCriteria.getPuMinute())
+                .selectDropOffTime(carBookingCriteria.getDoHour(), carBookingCriteria.getDoMinute())
+                .redirectToCarSelectionPage();
+        logger.log(Level.INFO, "Car selection page performed.");
+        return new CarSelectionPage(driver);
+    }
+
+    public HomePage clickSignInButton(){
+        login.click();
+        return this;
+    }
+
+    public String getLoginErrorMessage(){
+        return errorMessage.getText();
+    }
+
+    public HomePage selectDriversAge(String driversAge) {
+        driversAgeButton.click();
+        driver.findElement(By.xpath("//input[@id='driver-age-input']")).sendKeys(driversAge);
+        return this;
+    }
+
+    public HomePage inputPickUpLocation(String pickLoc) {
         pickUpLocation.sendKeys(pickLoc);
+        logger.info("Filled 'Pick-up location' field with " + pickLoc);
+        return this;
+    }
+
+    public HomePage inputDropOffLocation(String dropLoc) {
+        dropOffLocationButton.click();
+        pickUpLocation.sendKeys(dropLoc);
+        logger.info("Filled 'Drop Off location' field with " + dropLoc);
         return this;
     }
 
     public HomePage selectPickUpDate(String picDate) {
         dropOffDate.sendKeys(picDate);
+        logger.info("Filled 'Pick-up date' field with " + picDate);
         return this;
     }
 
     public HomePage selectDropOffDate(String dropDate) {
         dropOffDate.sendKeys(dropDate);
+        logger.info("Filled 'Drop off date' field with " + dropDate);
         return this;
     }
 
-    public CarSelectionPage clickCorrectSearch() {
+    public CarSelectionPage redirectToCarSelectionPage() {
         searchButton.click();
         return new CarSelectionPage(driver);
     }
 
-    public HomePage clickSearch() {
+    public HomePage redirectToHomePage() {
         searchButton.click();
+        logger.log(Level.INFO, "Car selection page performed.");
         return this;
     }
 
